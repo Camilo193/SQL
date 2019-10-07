0734a67b7d1bc8922f1f09061dfce85f866bd648
Juan Camilo <juanus196@gmail.com>
2019-10-07 06:38:24 -0500

    Add files via upload

---
+++ 04_Objetos de Base de datos/06_Trigger base de datos/DDL DATABASE.sql
@@ -0,0 +1,60 @@
+--Creamos un TRIGGER que no nos deja eliminar o alterar tablas
+CREATE TRIGGER safety   
+ON DATABASE   
+FOR DROP_TABLE, ALTER_TABLE   
+AS   
+   PRINT 'You must disable Trigger "safety" to drop or alter tables!'   
+   ROLLBACK;
+--------------------------------------------------------------------------
+--Intentamos eliminar una tabla
+DROP TABLE Application.SystemParameters
+GO
+--No nos dejará eliminar la tabla
+---------------------------------------------------------------------------
+--Modificamos el TRIGGER para que tampoco nos deje crear tablas
+ALTER TRIGGER safety   
+ON DATABASE   
+FOR DROP_TABLE, ALTER_TABLE, CREATE_TABLE   
+AS   
+   PRINT 'You must disable Trigger "safety" to drop, alter or create tables!'   
+   ROLLBACK;
+---------------------------------------------------------------------------------
+--Intentamos crear una tabla
+
+CREATE TABLE TEST
+( Code NCHAR(4), Category NVARCHAR(100) )
+GO
+----------------------------------------------------------------------------------------------
+--Deshabilitamos el TRIGGER
+DISABLE TRIGGER safety ON DATABASE
+----------------------------------------------------------------------------------------------
+--Intentamos crear la tabla nuevamente
+CREATE TABLE TEST
+( Code NCHAR(4), Category NVARCHAR(100) )
+GO
+----------------------------------------------------------------------------------------------
+--Eliminamos la tabla
+DROP TABLE TEST
+----------------------------------------------------------------------------------------------
+--Eliminamos el TRIGGER
+DROP TRIGGER safety ON DATABASE;
+----------------------------------------------------------------------------------------------
+--Creamos un TRIGGER que capture el evento cuando se cree una vista
+CREATE TRIGGER GetView ON DATABASE
+FOR Create_View
+AS
+BEGIN
+SELECT EVENTDATA()
+END
+GO
+----------------------------------------------------------------------------------------------
+--Creamos una vista y nos devuelve un esquema XML con los detalles
+CREATE VIEW ViewCategories
+AS
+SELECT C.SupplierCategoryID As 'Code', C.SupplierCategoryName As 'Name'
+FROM Purchasing.SupplierCategories As C
+GO
+--------------------------------------------------------------------------------------------
+--Limpiamos
+DROP TRIGGER GetView ON DATABASE;
+DROP VIEW ViewCategories

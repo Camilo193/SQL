22035ac5d50743aad55191fe93c9e527858df268
Juan Camilo <juanus196@gmail.com>
2019-10-07 11:36:37 -0500

    Acceso a base de datos completo

---
+++ 02_Acceso a base de datos/03_Uso de parámetros tipo tabla/TableValueParameter/TableValueParameter/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace TableValueParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+
+
+            {
+                string cs = "Data Source=DESKTOP-O3RV5AC;Initial Catalog=WideWorldImporters; Integrated Security = True";
+                
+                DataTable dt = new DataTable("TableToInsert");
+                dt.Columns.Add("StockItemTransactionID", typeof(int));
+                dt.Columns.Add("StockItemID", typeof(int));
+                dt.Columns.Add("TransactionTypeID", typeof(int));
+                dt.Columns.Add("CustomerID", typeof(int));
+                dt.Columns.Add("InvoiceID", typeof(int));
+                dt.Columns.Add("SupplierID", typeof(int));
+                dt.Columns.Add("PurchaseOrderID", typeof(int));
+                dt.Columns.Add("TransactionOccurredWhen", typeof(DateTime));
+                dt.Columns.Add("Quantity", typeof(decimal));
+                dt.Columns.Add("LastEditedBy", typeof(int));
+                dt.Columns.Add("LastEditedWhen", typeof(DateTime));
+
+                using (SqlConnection con = new SqlConnection(cs))
+
+
+                {
+                    
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("EXECUTE GetTransaction", con);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}", 
+                            rdr["StockItemTransactionID"], rdr["StockItemID"], rdr["TransactionTypeID"], rdr["CustomerID"], 
+                            rdr["InvoiceID"], rdr["SupplierID"], rdr["PurchaseOrderID"], rdr["TransactionOccurredWhen"], 
+                            rdr["Quantity"], rdr["LastEditedBy"], rdr["LastEditedWhen"]);
+
+                    rdr.Close();
+                    
+                    
+
+                    // Insertamos
+                    dt.Rows.Add(new object[] {29123468, 222, 10, null, null, null, null, DateTime.Now, 12, 2, DateTime.Now});
+                    // Creamos un parametro
+                    SqlParameter tvpParam = new SqlParameter();
+                    tvpParam.ParameterName = "@TVP";
+                    tvpParam.Value = dt;
+                    tvpParam.SqlDbType = SqlDbType.Structured;
+
+                    tvpParam.TypeName = "TVPStockItemTransactions";
+                    SqlCommand tvpcmd = new SqlCommand("TVPProcedure", con);
+                    tvpcmd.CommandType = CommandType.StoredProcedure;
+                    tvpcmd.Parameters.Add(tvpParam);
+                    tvpcmd.ExecuteNonQuery();
+
+                    rdr = cmd.ExecuteReader();
+                    //Lleemos los datos en la consola
+                    while (rdr.Read())
+                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}",
+                            rdr["StockItemTransactionID"], rdr["StockItemID"], rdr["TransactionTypeID"], rdr["CustomerID"],
+                            rdr["InvoiceID"], rdr["SupplierID"], rdr["PurchaseOrderID"], rdr["TransactionOccurredWhen"],
+                            rdr["Quantity"], rdr["LastEditedBy"], rdr["LastEditedWhen"]);
+
+                    rdr.Close();
+                    con.Close();
+
+
+                }
+
+
+            }
+
+
+            catch (Exception e)
+
+
+            {
+
+
+                Console.WriteLine(e);
+
+
+            }
+        }
+    }
+}
---
+++ 02_Acceso a base de datos/07_IndexOutOfRangeException/IndexOutOfRangeException/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace IndexOutOfRangeException
+{
+    class Program
+    {
+        public static void test1()
+        {
+            int[] array = { 3, 6, 9, 12, 15, 18, 21 }; //7 elementos
+            int[] newArray = new int[6]; //6 elementos 
+            //Asignamos el último elemento del array al nuevo array
+            newArray[array.Length - 1] = array[array.Length - 1];
+        }
+
+        public static void test2()
+        {
+            Array values = Array.CreateInstance(typeof(int), new int[] { 10 },
+                                          new int[] { 1 });
+            int value = 2;
+            //Asigna los valores .
+            for (int ctr = 0; ctr < values.Length; ctr++)
+            {
+                values.SetValue(value, ctr);
+                value *= 2;
+            }
+
+            // Imprime los valores.
+            for (int ctr = 0; ctr < values.Length; ctr++)
+                Console.Write("{0}    ", values.GetValue(ctr));
+        }
+
+        static double safeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new System.DivideByZeroException();
+            return x / y;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            double a = 98, b = 0;
+            double result = 0;
+            try
+            {
+                safeDivision(a, b); //Como el primer método genera excepcion, no se ejecuta el segundo método
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+                test1();
+
+            }
+            //Los bloques catch deben ordenarse siempre de más específico a menos específico
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (System.IndexOutOfRangeException ex2)
+            {
+                //Mostramos la excepción y el número de la linea dónde se produjo la excepción
+                Console.WriteLine(ex2.Message + ex2.StackTrace);
+            }
+
+
+
+
+            try
+            {
+                test2();
+            }
+            catch (Exception ex)
+            {
+                //Mostramos la excepción y el número de la linea dónde se produjo la excepción
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
+
+
+
+        }
+
+
+    }
+}
+

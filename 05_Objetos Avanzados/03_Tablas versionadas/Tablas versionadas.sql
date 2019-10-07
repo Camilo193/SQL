f481e92e5852fee2e981e95bc910b57906394f3b
Juan Camilo <juanus196@gmail.com>
2019-10-07 06:33:53 -0500

    Add files via upload

---
+++ 05_Objetos Avanzados/03_Tablas versionadas/Tablas versionadas.sql
@@ -0,0 +1,53 @@
+USE WideWorldImporters
+GO
+------------------------------------------------------------------------------
+--Creamos una tabla nueva para nuestra prueba
+CREATE TABLE dbo.Department   
+(
+    DeptID INT NOT NULL PRIMARY KEY CLUSTERED
+  , DeptName VARCHAR(50) NOT NULL
+  , ManagerID INT NULL
+  , ParentDeptID INT NULL
+  , SysStartTime DATETIME2 GENERATED ALWAYS AS ROW START NOT NULL
+  , SysEndTime DATETIME2 GENERATED ALWAYS AS ROW END NOT NULL
+  , PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime)
+)
+--Agregamos la opción de tabla versionada
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.DepartmentHistory));
+------------------------------------------------------------------------------
+--Acá podemos activar o desactivar la opción
+ALTER TABLE dbo.Department
+SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.DepartmentHistory));
+GO
+------------------------------------------------------------------------------
+--Agregamos un dato a la tabla versionada
+
+INSERT INTO dbo.Department(
+           DeptID
+           ,DeptName
+           ,ManagerID
+           ,ParentDeptID)
+     VALUES
+           (1
+           ,'Florida'
+           ,1
+           ,10)
+GO
+-----------------------------------------------------------------------------
+--Hacemos un UPDATE en la tabla
+UPDATE	dbo.Department
+SET		DeptName = 'Alaska'
+WHERE	DeptID = 1
+-----------------------------------------------------------------------------
+--Hacemos un SELECT para visualizar todos los cambios que ha tenido
+SELECT	* 
+FROM	dbo.Department 
+FOR SYSTEM_TIME ALL --Periodos de tiempo
+--También se puede usar
+-- FOR SYSTEM_TIME BETWEEN @StartDateTime AND @EndDateTime
+-- FOR SYSTEM_TIME CONTAINED IN (@StartDateTime, @EndDateTime)
+-- FOR SYSTEM_TIME FROM @StartDateTime TO @EndDateTime
+-- FOR SYSTEM_TIME AS OF @Date
+WHERE	DeptID = 1
+
+
